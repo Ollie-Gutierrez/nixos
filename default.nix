0509e06f9d2dcc68b27075a689e55b62fb399c1f
Ollie Gutierrez <72130629+Ollie-Gutierrez@users.noreply.github.com>
2026-01-21 10:50:21 +0000

    Add files via upload

--- default.nix
+++ default.nix
@@ -1,13 +1,4 @@
-{ withSystem, inputs, ... }:
-let
-  inherit (inputs.self) lib;
-  inherit (lib) mkSystem;
-
-  hosts = lib.attrsets.mapAttrs' (name: _: {
-    name = name;
-    value = mkSystem (import ./${name} // { modules = [ ./${name} ]; });
-  }) (builtins.readDir ./.);
-in
 {
-  flake.nixosConfigurations = hosts;
+  hostname = "legion";
+  system = "x86_64-linux";
 }
